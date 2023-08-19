bf2e768b74d76229f8d5fb76f1e0bd7bcd4fec5c
Juan Esteban Rodríguez <juanesteban0130@gmail.com>
2023-08-18 22:32:09 -0500

    Se agregó un componente de barra de búsqueda

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import Header from './Componentes/Header-Menu/header-menu';
 import Footer from './Componentes/Footer/footer';
 import SectionContent from './Componentes/Section-Content/section-content';
+import SearchBar from './Componentes/SearchBar/SearchBar'; // Se importa el componente
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
             <img className='image-1'></img>
             <img className='image-2'></img>
             <Header />
+            <SearchBar /> {/* Se agrega el componente de búsqueda */}
             <SectionContent/>
             <Footer />
         </main>
