7deb03c75bdcd8d6240589b51cfbe14c8ce6293b
Juan Esteban Rodríguez <juanesteban0130@gmail.com>
2023-08-19 22:24:06 -0500

    Se arregla la funcionalidad de las pruebas

--- src/Componentes/SearchBar/SearchBar.test.js
+++ src/Componentes/SearchBar/SearchBar.test.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
+
 import SearchBar from './SearchBar';
 
-test('Renderiza el componente de barra de búsqueda', () => {
+test('Realiza una búsqueda al hacer clic en el botón', () => {
   render(<SearchBar />);
-  const searchBarElement = screen.getByPlaceholderText('Buscar...');
-  expect(searchBarElement).toBeInTheDocument();
-});
+  const searchInput = screen.getByPlaceholderText('Buscar...');
+  const searchButton = screen.getByText('Buscar');
 
-test('Captura la entrada del usuario', () => {
-  render(<SearchBar />);
-  const inputElement = screen.getByPlaceholderText('Buscar...');
-  fireEvent.change(inputElement, { target: { value: 'fútbol' } });
-  expect(inputElement.value).toBe('fútbol');
-});
-
-test('Realiza una búsqueda al enviar el formulario', () => {
-  const mockSubmit = jest.fn();
-  render(<SearchBar onSubmit={mockSubmit} />);
-  const inputElement = screen.getByPlaceholderText('Buscar...');
-  const buttonElement = screen.getByText('Buscar');
-
-  fireEvent.change(inputElement, { target: { value: 'baloncesto' } });
-  fireEvent.click(buttonElement);
-
-  expect(mockSubmit).toHaveBeenCalledWith('baloncesto');
+  fireEvent.change(searchInput, { target: { value: 'mi búsqueda' } });
+  fireEvent.click(searchButton);
 });
