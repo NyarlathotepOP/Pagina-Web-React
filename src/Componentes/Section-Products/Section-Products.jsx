d0aa7d4cebfa0a215298ba08c1d5f0a6d2ddd538
Diego Alonzo Gutierrez <diego.alonzo9090@gmail.com>
2023-08-27 13:41:12 -0500

    Correcciones sobre el footer y losproductos.

--- src/Componentes/Section-Products/Section-Products.jsx
+++ src/Componentes/Section-Products/Section-Products.jsx
@@ -12,53 +12,32 @@ import ImageGuantes from '../../Images/guantes.jpg'
 import ImageBinoculares from '../../Images/binoculares.jpg'
 
 export default function SectionProducts() {
+    const products = [
+        { name: "Zapatillas", image: ImageZapatillas },
+        { name: "Camisas", image: ImageCamisas },
+        { name: "Pesas", image: ImagePesas },
+        { name: "Equipo", image: ImageEquipo },
+        { name: "Caminadoras", image: ImageCaminadoras },
+        { name: "Pantalones", image: ImagePantalones },
+        { name: "Morrales", image: ImageMorrales },
+        { name: "Termos", image: ImageTermos },
+        { name: "Guantes", image: ImageGuantes },
+        { name: "Binoculares", image: ImageBinoculares }
+    ];
+
     return (
         <section className="info" id="servicios">
             <section className="ropa container">
                 <h2>Ropa y Accesorios</h2>
                 <div className="ropa-content">
-                    <div className="ropa-1">
-                        <h3>Zapatillas</h3>
-                        <img src={ImageZapatillas} alt="Zapatillas" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Camisas</h3>
-                        <img src={ImageCamisas} alt="Camisas" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Pesas</h3>
-                        <img src={ImagePesas} alt="Pesas" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Equipo</h3>
-                        <img src={ImageEquipo} alt="Equipo" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Caminadoras</h3>
-                        <img src={ImageCaminadoras} alt="Caminadoras" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Pantalones</h3>
-                        <img src={ImagePantalones} alt="Pantalones" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Morrales</h3>
-                        <img src={ImageMorrales} alt="Morrales" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Termos</h3>
-                        <img src={ImageTermos} alt="Termos" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Guantes</h3>
-                        <img src={ImageGuantes} alt="Guantes" />
-                    </div>
-                    <div className="ropa-1">
-                        <h3>Binoculares</h3>
-                        <img src={ImageBinoculares} alt="Binoculares" />
-                    </div>
+                    {products.map((product, index) => (
+                        <div className="ropa-1" key={index}>
+                            <h3>{product.name}</h3>
+                            <img src={product.image} alt={product.name} />
+                        </div>
+                    ))}
                 </div>
             </section>
-        </section >
+        </section>
     );
 }
