84350212a278fc5406838f6f4dc04a8993e85d6f
Diego Alonzo Gutierrez <diego.alonzo9090@gmail.com>
2023-08-27 13:26:07 -0500

    Coreccion en la seccion de productos.

--- src/Componentes/Section-Products/Section-Products.jsx
+++ src/Componentes/Section-Products/Section-Products.jsx
@@ -13,47 +13,47 @@ import ImageBinoculares from '../../Images/binoculares.jpg'
 
 export default function SectionProducts() {
     return (
-        <section class="info" id="servicios">
-            <section class="ropa container">
+        <section className="info" id="servicios">
+            <section className="ropa container">
                 <h2>Ropa y Accesorios</h2>
-                <div class="ropa-content">
-                    <div class="ropa-1">
+                <div className="ropa-content">
+                    <div className="ropa-1">
                         <h3>Zapatillas</h3>
                         <img src={ImageZapatillas} alt="Zapatillas" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Camisas</h3>
                         <img src={ImageCamisas} alt="Camisas" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Pesas</h3>
                         <img src={ImagePesas} alt="Pesas" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Equipo</h3>
                         <img src={ImageEquipo} alt="Equipo" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Caminadoras</h3>
                         <img src={ImageCaminadoras} alt="Caminadoras" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Pantalones</h3>
                         <img src={ImagePantalones} alt="Pantalones" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Morrales</h3>
                         <img src={ImageMorrales} alt="Morrales" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Termos</h3>
                         <img src={ImageTermos} alt="Termos" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Guantes</h3>
                         <img src={ImageGuantes} alt="Guantes" />
                     </div>
-                    <div class="ropa-1">
+                    <div className="ropa-1">
                         <h3>Binoculares</h3>
                         <img src={ImageBinoculares} alt="Binoculares" />
                     </div>
