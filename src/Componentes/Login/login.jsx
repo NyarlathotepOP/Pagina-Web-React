b759c60bdacad14b37d422087a9fd9eeae50c2af
YEIDER FRANCO FRANCO <140337289+yeifrancoo77@users.noreply.github.com>
2023-08-29 01:07:59 -0500

    Update Login.jsx
    Estructura del componente de inicio de sesión

--- src/Componentes/Login/login.jsx
+++ src/Componentes/Login/login.jsx
@@ -1,9 +1,26 @@
-export default function Login () {
+export default function Login() {
   return (
     <div>
+      <form>
+        <h2>Iniciar sesión</h2>
         <div>
-            <h1>INCIO DE SESIÓN</h1>
+          <label>Email:</label>
+          <input type="email" />
         </div>
+        <div>
+          <label>Contraseña:</label>
+          <input type="password" />
+        </div>
+        <button>Iniciar sesión</button>
+      </form>
+      <div>
+        <p>O continuar con:</p>
+        <button>
+          <img src="https://cdn.worldvectorlogo.com/logos/google-g-2015.svg" width={30} alt="Google icon" />
+          Iniciar sesión con Google
+        </button>
+      </div>
     </div>
-  )
-} 
+  );
+}
+
