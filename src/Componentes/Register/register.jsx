372275f123d696a2482e2cea118c56b280371a02
YEIDER FRANCO FRANCO <140337289+yeifrancoo77@users.noreply.github.com>
2023-08-29 01:55:38 -0500

    Estructure Register

--- src/Componentes/Register/register.jsx
+++ src/Componentes/Register/register.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>
-        THIS IS REGISTER
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h2 className={styles.formHeader}>Crear una cuenta</h2>
+        <div className={styles.inputGroup}>
+          <label className={styles.label}>Nombre:</label>
+          <input type="text" className={styles.input} />
+        </div>
+        <div className={styles.inputGroup}>
+          <label className={styles.label}>Email:</label>
+          <input type="email" className={styles.input} />
+        </div>
+        <div className={styles.inputGroup}>
+          <label className={styles.label}>Contraseña:</label>
+          <input type="password" className={styles.input} />
+        </div>
+        <div className={styles.inputGroup}>
+          <label className={styles.label}>Confirmar contraseña:</label>
+          <input type="password" className={styles.input} />
+        </div>
+        <button className={styles.registerButton}>Registrarse</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
